9cc87e4ba294befdb8fac70cbf2a3e184b6e4005
Jan Lesjak <if23b215@technikum-wien.at>
2025-06-20 07:35:19 +0200

    Add test for item SellIn decrement

--- csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
@@ -23,6 +23,17 @@ public class GildedRoseTest
 
         app.UpdateQuality();
 
-        Assert.That(items[0].Quality, Is.EqualTo(9));
+        Assert.That(items[0].Quality, Is.EqualTo(9), "Item Quality should decrease by 1 each day.");
+    }
+
+    [Test]
+    public void Item_SellIn_Decrements_By_One()
+    {
+        var items = new List<Item> { new Item { Name = "foo", SellIn = 5, Quality = 10 } };
+        var app = new GildedRose(items);
+
+        app.UpdateQuality();
+
+        Assert.That(items[0].SellIn, Is.EqualTo(4), "Item SellIn should decrease by 1 each day.");
     }
 }
